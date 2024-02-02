cdc3734ccffd22256395e414aabf7280ab6b7de0
Sophia Williams <sophiaashleighwilliams@gmail.com>
2024-02-02 12:05:45 -0500

    gitignore

--- server/server.js
+++ server/server.js
@@ -17,11 +17,11 @@ app.listen(PORT, () => {
 
 // if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/')));
+	app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../client/index.html'));
+	res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 // db.once('open', () => {
